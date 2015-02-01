1366d17bdcbea16394998212e7a2991c95e1b961
Kevin Lin <kevinlin135@yahoo.com>
2015-02-01 12:35:59 -0500

    Made login the default page and changed the title of the pages.

--- HillsboroughEducation/App_Start/RouteConfig.cs
+++ HillsboroughEducation/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HillsboroughEducation
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
