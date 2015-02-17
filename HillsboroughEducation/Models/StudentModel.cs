51cb7567657749935d1a6dbfd69deb8dbad7b589
Kevin Lin <kevinlin135@yahoo.com>
2015-02-17 18:17:56 -0500

    Missed some files in the last commit

---
+++ HillsboroughEducation/Models/StudentModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace HillsboroughEducation.Models
+{
+    [Table("StudentProfile")]
+    public class StudentModel
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+    }
+}
