5f105f876e712f46a813df6cd86cde52ce815cfa
Kevin Lin <kevinlin135@yahoo.com>
2015-02-19 18:18:06 -0500

    Fixed the Search Function

--- HillsboroughEducation/Controllers/AdminController.cs
+++ HillsboroughEducation/Controllers/AdminController.cs
@@ -21,47 +21,42 @@ namespace HillsboroughEducation.Controllers
 
         //
         // GET: /Admin/Student
-        [HttpGet]
-        public ActionResult Student(int sortBy = 1, bool isAsc = true)
+        public ActionResult Student(int sortBy = 1)
         {
-            IEnumerable<StudentModel> students;
-            var studentlist = db.StudentProfiles.ToList();
-            
+            var students = from s in db.StudentProfiles
+                           select s;
             #region Sorting
             switch (sortBy)
             {
                 case 1:
-                    students = isAsc ? studentlist.OrderBy(s => s.LastName) : studentlist.OrderByDescending(s => s.LastName);
+                    students = students.OrderBy(s => s.LastName);
                     break;
 
                 case 2:
-                    students = isAsc ? studentlist.OrderBy(s => s.FirstName) : studentlist.OrderByDescending(s => s.FirstName);
+                    students = students.OrderBy(s => s.FirstName);
                     break;
 
                 case 3:
-                    students = isAsc ? studentlist.OrderBy(s => s.StudentNumber) : studentlist.OrderByDescending(s => s.StudentNumber);
+                    students = students.OrderBy(s => s.StudentNumber);
                     break;
 
                 case 4:
-                    students = isAsc ? studentlist.OrderBy(s => s.UserName) : studentlist.OrderByDescending(s => s.UserName);
+                    students = students.OrderBy(s => s.UserName);
                     break;
 
                 case 5:
-                    students = isAsc ? studentlist.OrderBy(s => s.BirthDate) : studentlist.OrderByDescending(s => s.BirthDate);
+                    students = students.OrderBy(s => s.BirthDate);
                     break;
 
                 case 6:
-                    students = isAsc ? studentlist.OrderBy(s => s.AcademicYear) : studentlist.OrderByDescending(s => s.AcademicYear);
+                    students = students.OrderBy(s => s.AcademicYear);
                     break;
 
                 default:
-                    students = isAsc ? studentlist.OrderBy(s => s.LastName) : studentlist.OrderByDescending(s => s.LastName);
+                    students = students.OrderBy(s => s.LastName);
                     break;
             }
             #endregion
-
-            ViewBag.SortBy = sortBy;
-            ViewBag.IsAsc = isAsc;
 
             return View(students);
         }
@@ -71,12 +66,16 @@ namespace HillsboroughEducation.Controllers
         [HttpPost]
         public ActionResult Student(string searchString)
         {
-            var students = from s in db.StudentProfiles select s;
-            students = students.Where(s => (s.UserName.Contains(searchString)) || 
-                                           (s.LastName.Contains(searchString)) ||
-                                           (s.MiddleName.Contains(searchString)) ||
-                                           (s.FirstName.Contains(searchString)) ||
-                                           (s.StudentNumber.Contains(searchString))).OrderBy(s => s.LastName);
+
+            var students = from s in db.StudentProfiles
+                           select s;
+
+            students = students.Where(s => (s.UserName.Contains(searchString)) ||
+                                            (s.LastName.Contains(searchString)) ||
+                                            (s.MiddleName.Contains(searchString)) ||
+                                            (s.FirstName.Contains(searchString)) ||
+                                            (s.StudentNumber.Contains(searchString)) ||
+                                            (s.AcademicYear.Contains(searchString))).OrderBy(s => s.LastName);
 
             return View(students);
         }
