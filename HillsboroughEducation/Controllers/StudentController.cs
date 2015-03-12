423f7bc5aa43aa076577109231e7f7de434e9ea4
Kcousine <doctorfrogman@gmail.com>
2015-03-12 11:09:09 -0400

    Student

---
+++ HillsboroughEducation/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HillsboroughEducation.Models;
+
+namespace HillsboroughEducation.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        private UsersContext db = new UsersContext();
+        //
+        // GET: /Student/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Student/MyInfo
+        public ActionResult MyInfo()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Student/Scholarship
+        public ActionResult Scholarship()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Student/Applications
+        public ActionResult Application()
+        {
+            return View();
+        }
+
+    }
+
+}
