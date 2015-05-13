c7619de12a6ab22626fb58aac302fef15b69ae87
Sean Farley <seanfarley24@gmail.com>
2015-05-12 23:57:00 -0400

    added loadQuestions DML

---
+++ loadQuestions.sql
@@ -0,0 +1,141 @@
+﻿INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are either of your parents currently or have they ever been incarcerated?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are either of your parents.guardians unemployed?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are you a Hillsborough County Public Schools permanent CTAE instructional employee and a current member of the Hillsborough Technical Center & Adult Association (HTCAA)?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are you a student athlete?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are you a U.S. Citizen or Legal U.S. resident?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are you a non-instructional support employee that plans on attending HCC to major in educations then teach within Hillsborough County Public School system upon graduation?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are either of your parents currently or have they ever been incarcerated?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are you currently enrolled in a Teen Parent Program?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are you currently in a music program at your high school?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are you currently in or have you ever been in foster care?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are you homeless or living with extended family or friends?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are you in the special diploma program?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are you on schedule to graduate this school year with either a performance-based diploma or a state diploma?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Did you participate in the Strides for Education 5k Walk held on November 1 2014 to help create a scholarship for your school?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Do either of your parents/guardians have a disability?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Do you have a disability?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Do you attend a charter school?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Do you have 3 or more siblings who currently live with you?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Do you have an active or retired military parent?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are either of your parents currently or have they ever been incarcerated?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Do you have extended family (Uncles Aunts Grandparents etc.) living in your home?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Do you live in a single parent household?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Do you live in a single parent/guardian household?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Do you plan on attending a college, university, or technical center in Hillsborough county?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Do you qualify for free or reduced lunch?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Do you volunteer in your community?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Does anyone in your household currently suffer from a serious/terminal illness?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Does at least one of your parents work for Hillsborough County Public Schools?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Does your bus ride to school take 30 minutes or longer?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Has either of your parents/guardians completed a 2 year or 4 year college degree?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Has either of your parents/guardians graduated from High School?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Has the Department of Children & Families been involved with your family in the past 12 months?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Has you family received TANF benefits from the State of Florida within the last 12 months?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'In what grade are you?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Is anyone in your immediate family a Migrant Farm Worker?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Is English a 2nd language in your household?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Is your family''s current residence (home) in forclosure', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are either of your parents currently or have they ever been incarcerated?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Is your GPA 2.5 or above?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Is your GPA 2.7 or above?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Are either of your parents currently or have they ever been incarcerated?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Is your parent assigned to MacDill Air Force Base?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Is your parent/guardian an extended family member?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'My family''s annual income is les than $50,000?', 'NONE', NULL, NULL);
+
+INSERT INTO Questions(QUESTION_SEQ, DESCR, SCHL_TYPE, PREV_SEQ, NEXT_SEQ)
+VALUES (0, 'Were either of you parents teen parents?', 'NONE', NULL, NULL);
+
+
+
+
+SELECT * FROM Questions
+
+
