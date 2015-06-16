b609626d9ad0ccb86b91d9b0ef460f487cd1af62
Kevin Lin <kevinlin135@yahoo.com>
2015-06-16 16:40:56 -0400

    ScholarshipCriteria

---
+++ HillsboroughEducation/Models/ScholarshipCriteria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HillsboroughEducation.Models
+{
+    public class ScholarshipCriteriaContext : DbContext
+    {
+        public ScholarshipCriteriaContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<ScholarshipCriteria> ScholarshipCriteriaProfiles { get; set; }
+    }
+
+    [Table("ScholarshipCriteria")]
+    public class ScholarshipCriteria
+    {
+        public int ID { get; set; }
+        public int SCHOLARSHIP_ID { get; set; }
+        public int CRITERIA_ID { get; set; }
+    }
+
+    public class ScholarshipCriteriaModel
+    {
+        public ScholarshipModel scholarships { get; set; }
+        public List<Criteria> criterias { get; set; }
+    }
+}
